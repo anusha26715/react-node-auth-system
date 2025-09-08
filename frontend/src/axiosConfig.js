8c385c5af1411d50f09f54d113d93cd995decdec
anusha26715 <prathikantamanu@gmail.com>
2025-09-08 09:28:05 +0530

    added env file

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/auth", // backend base
-  withCredentials: true, // 👈 ensures cookies are sent
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/auth", 
+  withCredentials: true, // 👈 important for cookies/session
 });
 
 export default api;
